ad23ca369ee708373647496671d33d5a04249600
jo1201 <josuefaneittes@gmail.com>
2022-05-19 20:58:31 -0400

    Sigo

---
+++ src/pages/WelcomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout>
+    <q-page padding class="col items-center q-mb-lg">
+      <div class="text-h1 text-left">
+        Bienvenido, usuario
+      </div>
+      <div class="row text-h6 justify-left">
+          ¡Con CleanOps, vas a simplificar las actividades de mantenimiento,<br>
+          ahorrarando tiempo, tomarndo mejores decisiones y generarando<br>
+          mejores resultados!
+        <img class="responsive" src="~assets\assetsv0.2\Logo.svg" height="400" width="400" />
+      </div>
+    </q-page>
+  <q-footer>
+      <div class="text-center">Todos los derechos reservados © 2022</div>
+  </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,11 @@
 const routes = [
   {
     path: '/',
-    component: () => import('pages/Login.vue'),
-    /* children: [
-      { path:'Login',name:'Login-register',component:()=>import('src/layouts/Login.vue')},
-    ] */
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path:'Login',name:'Login-register',component:()=>import('pages/Login.vue')},
+      { path:'Welcome',name:'Welcome-page',component:()=>import('pages/WelcomePage.vue')}
+    ]
   },
 
   // Always leave this as last one,
